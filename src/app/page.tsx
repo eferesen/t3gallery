63ba31f7b2738068044c41c94034d5c383f5ffdc
Ahsan Imam <eferesen@gmsail.com>
2024-06-03 14:51:56 -0600

    Bump

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ const mockImage = mockUrls.map((url, index)=> ({
 export default async function HomePage() {
   const posts = await db.query.posts.findMany()
   console.log(posts)
+  // Adding a fake comment
   return (
     <main className=''>
       <div className='flex flex-wrap gap-4'>
